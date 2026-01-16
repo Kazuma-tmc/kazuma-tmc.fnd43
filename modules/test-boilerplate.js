175a5707651d672d2b69d23734294ad770c7d28e
Kazuma-tmc <kazuma_matsumoto_ab@mail.toyota.co.jp>
2026-01-16 12:57:30 +0900

    Add files via upload

---
+++ modules/test-boilerplate.js
@@ -0,0 +1,13 @@
+'use strict'
+// 1行目に記載している 'use strict' は削除しないでください
+
+function test(actual, expected) {
+  if (_.isEqual(actual, expected)) {
+    console.log("OK! Test PASSED.");
+  } else {
+    console.error("Test FAILED. Try again!");
+    console.log("    actual: ", actual);
+    console.log("  expected: ", expected);
+    console.trace();
+  }
+}
